cc28d33831f7f11c79bad74fabd643cb9ec2d6c5
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2022-12-29 23:11:53 +0100

    restructed classNames

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,20 +2,29 @@ import location from '../assets/location-icon.png'
 
 
 
+
 function Card(props) {
-    
+
     return (
-        <div className='card--section'>
-            <img className='card--img' src={props.item.img} alt="img" />
-            <img className='card--icon--location' src={location} alt="location-icon" />
-            <h3>{props.item.location}</h3>
-            <a className='card--link' href={props.item.mapLink}>View on Google Maps</a>
-            <div className='card--section--2'>
-                <h2>{props.item.sight}</h2>
+        <div className='card'>
+            <div>
+                <img className='card--img' src={props.item.image} alt="img" />
+            </div>
+
+            <div className='card--text--container'>
+                <div className='card--uppertext'>
+                    <img className='card--icon--location' src={location} alt="location-icon" />
+                    <h3 className='card--location'>{props.item.location}</h3>
+                    <a className='card--link' href={props.item.mapLink}>View on Google Maps</a>
+                </div>
+                <div className='card--lowertext'>
+                <h2 className='card--sight'>{props.item.sight}</h2>
                 <p className='card--p1'>{props.item.time}</p>
                 <p className='card--p2'>{props.item.describe}</p>
+                </div>
             </div>
         </div>
+
     )
 }
 
