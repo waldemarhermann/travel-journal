1f59e992a3b8af4bc9cd6142f2ffe2eaab211fbc
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2022-12-29 13:09:13 +0100

    add an array with 3 obj

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export default[
+    {
+        id: 1,
+        imgae: "grand-canyon.jpg",
+        location: "USA",
+        mapLink: "https://goo.gl/maps/GiokGeRKoh15JBf58",
+        sight: "Grand Canyon",
+        time: "01.05.2022 - 10.06.2022",
+        describe: "The Grand Canyon is a natural wonder located in Arizona, United States. It is a deep gorge carved by the Colorado River and is known for its stunning red rock formations and colorful layers of rock that reveal millions of years of Earth's history. The Grand Canyon is a popular tourist destination and is visited by millions of people each year. It is also a popular location for outdoor activities such as hiking, rafting, and camping.",
+    },
+    
+    {
+        id: 2,
+        image: "lofoten.jpg",
+        location: "Norway",
+        mapLink: "https://goo.gl/maps/p7gcfpcrPUNjzKWG8",
+        sight: "Lofoten",
+        time: "20.06.2022 - 07.07.2022",
+        describe: "The Lofoten Islands are a group of islands located in the Arctic Circle in northern Norway. The islands are known for their stunning natural beauty, with rugged coastline, fjords, and mountains. The Lofoten Islands are popular for outdoor activities such as hiking, fishing, and skiing, and are also home to a number of small villages and fishing communities. The islands have a unique culture and history, and are home to a number of traditional Norwegian wooden houses and fishing huts. The Lofoten Islands are a popular tourist destination due to their beauty and unique location in the Arctic."
+    },
+    
+    {
+        id: 3,
+        image: "dolomiten.jpg",
+        location: "Italy",
+        mapLink: "https://goo.gl/maps/Dz6xapzmpi3QB5Bt5",
+        sight: "Dolomiten",
+        time: "02.08.2022 - 21.08.2022",
+        describe: "The Dolomites are a mountain range located in northeastern Italy. The range is part of the Italian Alps and is known for its rugged, jagged peaks and beautiful natural scenery. The Dolomites are popular for a variety of outdoor activities, including hiking, climbing, and skiing, and are also home to a number of small villages and resorts. The range has been designated a UNESCO World Heritage site due to its unique geology and natural beauty."
+    },
+]
+
+
+
+
