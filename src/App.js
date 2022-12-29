77bc16d2ba27cf1ffabf8019dc3a00b036d6bf96
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2022-12-29 13:08:58 +0100

    add map-function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar"
 import Card from './components/Card'
+import data from './data'
+
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card
+        key={item.id}
+        img={item.image}
+        location={item.location}
+        mapLink={item.mapLink}
+        sight={item.sight}
+        time={item.sight}
+        descripe={item.describe}
+      />
+    )
+  })
+
   return (
     <div>
       <Navbar />
-      <Card />
+      {cards}
     </div>
   );
 }
